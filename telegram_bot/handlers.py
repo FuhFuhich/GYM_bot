1dbafb4fce2208b071eb7f462b8fd9d1d4293c7f
Nastya <1764347@bk.ru>
2024-03-21 22:39:32 +0300

    first commit berchik

--- telegram_bot/handlers.py
+++ telegram_bot/handlers.py
@@ -1,34 +1,51 @@
 from aiogram import types
-from bot_setup import dp
+from bot_setup import dp, bot
 from states import ChatGPTState
-from keyboards import create_reply_keyboard, create_question_keyboard
 from openai_api import get_chatgpt_response
 from aiogram.dispatcher import FSMContext
-
+from keyboards import *
 
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
-    await message.reply("Привет, {}! Я тестовый бот для твоей статьи для habr.com".format(message.from_user.first_name),
+    await message.answer("Привет, {}! Я бот для создания тренировок с помощью ИИ".format(message.from_user.first_name),
                         reply_markup=create_reply_keyboard())
+    await message.answer('Выберите группу мышц которую хотите тренировать:', reply_markup=muscles_keyboard())
 
 
 @dp.message_handler()
 async def process_button(message: types.Message):
+    # это нужно почистить
     if message.text == "Поздороваться":
         await message.answer("Привеет.. Спасибо что читаешь статью!")
     elif message.text == "Задать вопрос":
-        await message.answer("Задай мне вопрос", reply_markup=create_question_keyboard())
+        await message.answer("Задай мне вопрос")
     elif message.text == "Как меня зовут?":
         await message.answer("У меня нет имени..")
     elif message.text == "Что я могу?":
         await message.answer("Поздороваться с читателями")
+
     elif message.text == "Вернуться в главное меню":
         await message.answer("Вы вернулись в главное меню", reply_markup=create_reply_keyboard())
+
     elif message.text == "Обратиться к ИИ":
         await ChatGPTState.question.set()
         await message.answer("Задайте вопрос для ChatGPT")
     else:
         await message.answer("На такую комманду я не запрограммировал..")
+
+#эти два хендлера пока не работают нормально
+
+@dp.callback_query_handler(lambda c: c.data == 'button1')
+async def process_callback_button1(callback_query: types.CallbackQuery):
+    await bot.answer_callback_query(callback_query.id)
+    await bot.send_message(callback_query.from_user.id, 'Нажата первая кнопка!', reply_markup=muscles_keyboard())
+    # await callback_query.message.delete_reply_markup()
+
+
+@dp.callback_query_handler(lambda c: c.data == 'button2')
+async def process_callback_button2(callback_query: types.CallbackQuery):
+    await bot.answer_callback_query(callback_query.id)
+    await bot.send_message(callback_query.from_user.id, 'Нажата вторая кнопка!')
 
 
 @dp.message_handler(state=ChatGPTState.question)
--- telegram_bot/keyboards.py
+++ telegram_bot/keyboards.py
@@ -1,19 +1,33 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import ReplyKeyboardRemove, \
+    ReplyKeyboardMarkup, KeyboardButton, \
+    InlineKeyboardMarkup, InlineKeyboardButton
 
 
 def create_reply_keyboard():
     keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-    button1 = KeyboardButton("Поздороваться")
-    button2 = KeyboardButton("Задать вопрос")
-    button3 = KeyboardButton("Обратиться к ИИ")
-    keyboard.add(button1, button2, button3)
+    button1 = KeyboardButton("Главное меню")
+    keyboard.add(button1)
     return keyboard
 
+def muscles_keyboard():
+    inline_btn_1 = InlineKeyboardButton('Бицепс', callback_data='button1')
+    inline_btn_2 = InlineKeyboardButton('Трицепс', callback_data='button2')
+    inline_btn_3 = InlineKeyboardButton('Плечи', callback_data='button3')
+    inline_btn_4 = InlineKeyboardButton('Предпечья', callback_data='button4')
+    inline_btn_5 = InlineKeyboardButton('Грудь', callback_data='button5')
+    inline_btn_6 = InlineKeyboardButton('Пресс', callback_data='button6')
+    inline_btn_7 = InlineKeyboardButton('Спина', callback_data='button7')
+    inline_btn_8 = InlineKeyboardButton('Квадрицепсы', callback_data='button8')
+    inline_btn_9 = InlineKeyboardButton('Задняя поверхность бедра', callback_data='button9')
+    inline_btn_10 = InlineKeyboardButton('Икроножные', callback_data='button10')
+    inline_btn_11 = InlineKeyboardButton('Ягодицы', callback_data='button11')
+    inline_kb_muscles = InlineKeyboardMarkup(row_width=1)
+    inline_kb_muscles.add(inline_btn_1, inline_btn_2, inline_btn_3, inline_btn_4, inline_btn_5, inline_btn_6, inline_btn_7, inline_btn_8, inline_btn_9, inline_btn_11, inline_btn_10)
+    return inline_kb_muscles
 
-def create_question_keyboard():
-    keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-    btn1 = KeyboardButton("Как меня зовут?")
-    btn2 = KeyboardButton("Что я могу?")
-    back = KeyboardButton("Вернуться в главное меню")
-    keyboard.add(btn1, btn2, back)
-    return keyboard
+def bicep():
+    inline_btn_1_bicep = InlineKeyboardButton('Упражнение 1', callback_data='button1_bicep')
+    inline_btn_2_bicep = InlineKeyboardButton('Упражнение 2', callback_data='button2_bicep')
+    inline_btn_3_bicep = InlineKeyboardButton('Упражнение 3', callback_data='button3_bicep')
+    inline_kb_bicep = InlineKeyboardMarkup().add(inline_btn_1_bicep, inline_btn_2_bicep, inline_btn_3_bicep)
+    return inline_kb_bicep
