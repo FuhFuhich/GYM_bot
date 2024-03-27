1034e3bd0c9fb42f1edb500fbbbe22cdf465e3d1
Your Name <artembes.cs@gmail.com>
2024-03-27 20:57:24 +0300

    Кнопки

--- telegram_bot/handlers.py
+++ telegram_bot/handlers.py
@@ -1,15 +1,21 @@
+import copy
+
 from aiogram import types
 from bot_setup import dp, bot
 from states import ChatGPTState
 from openai_api import get_chatgpt_response
 from aiogram.dispatcher import FSMContext
 from keyboards import *
+from aiogram import types
 
+
+global flag
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
     await message.answer("Привет, {}! Я бот для создания тренировок с помощью ИИ".format(message.from_user.first_name),
                         reply_markup=create_reply_keyboard())
     await message.answer('Выберите группу мышц которую хотите тренировать:', reply_markup=muscles_keyboard())
+
 
 
 @dp.message_handler()
@@ -33,12 +39,79 @@ async def process_button(message: types.Message):
     else:
         await message.answer("На такую комманду я не запрограммировал..")
 
-#эти два хендлера пока не работают нормально
-
+bicepArray = []
 @dp.callback_query_handler(lambda c: c.data == 'button1')
 async def process_callback_button1(callback_query: types.CallbackQuery):
     await bot.answer_callback_query(callback_query.id)
-    await bot.send_message(callback_query.from_user.id, 'Нажата первая кнопка!', reply_markup=muscles_keyboard())
+    await bot.send_message(callback_query.from_user.id, 'Выберите упражнение', reply_markup=bicep())
+
+
+
+@dp.callback_query_handler(lambda c: c.data == 'button1_bicep')                         # 1е упражнение для бицепса
+async def process_callback_button1_bicep(callback_query: types.CallbackQuery):
+    global flag                                                                         # flag нужен для того чтобы после выбора 2й раз одного и того же упражнения запоминалось, какое полседнее упр было чтобы его удалить
+    flag = 1
+    await bot.send_message(callback_query.from_user.id, 'Выбрано первое упражнение')
+    await bot.answer_callback_query(callback_query.id)
+    if 1 not in bicepArray:                                                             # bicepArray - массив который содержит выбранные упражнения, этот if не дает 2 раза записать одно и то же упражнение в массив
+        bicepArray.append(1)
+        await bot.send_message(callback_query.from_user.id, 'Выберите упражнение', reply_markup=muscles_keyboard())
+    else:
+        await bot.send_message(callback_query.from_user.id,'Вы уже выбрали это упражнение. Хотите его убрать?', reply_markup=deleteExercise())          # если два раза выбрано одно и то же, предлагаем удалить упражнение (да/нет)
+        await bot.answer_callback_query(callback_query.id)
+    print("Упражнения для бицепса: ", *bicepArray)                                      # вывод в консоль выбранных упражнений
+
+
+
+@dp.callback_query_handler(lambda c: c.data == 'button2_bicep')                         # 2е упражнение для бицепса
+async def process_callback_button1_bicep(callback_query: types.CallbackQuery):
+    global flag                                                                         # дальше по аналогии с 1м.......................
+    flag = 2
+    await bot.send_message(callback_query.from_user.id, 'Выбрано второе упражнение')
+    await bot.answer_callback_query(callback_query.id)
+    if 2 not in bicepArray:
+        bicepArray.append(2)
+        await bot.send_message(callback_query.from_user.id, 'Выберите упражнение', reply_markup=muscles_keyboard())
+    else:
+        await bot.send_message(callback_query.from_user.id,'Вы уже выбрали это упражнение. Хотите его убрать?', reply_markup=deleteExercise())
+        await bot.answer_callback_query(callback_query.id)
+    print("Упражнения для бицепса: ", *bicepArray)
+
+
+
+@dp.callback_query_handler(lambda c: c.data == 'button3_bicep')
+async def process_callback_button1_bicep(callback_query: types.CallbackQuery):
+    global flag
+    flag = 3
+    await bot.send_message(callback_query.from_user.id, 'Выбрано третье упражнение')
+    await bot.answer_callback_query(callback_query.id)
+    if 3 not in bicepArray:
+        bicepArray.append(3)
+        await bot.send_message(callback_query.from_user.id, 'Выберите упражнение', reply_markup=muscles_keyboard())
+    else:
+        await bot.send_message(callback_query.from_user.id,'Вы уже выбрали это упражнение. Хотите его убрать?', reply_markup=deleteExercise())
+        await bot.answer_callback_query(callback_query.id)
+
+    print("Упражнения для бицепса: ", *bicepArray)
+
+
+@dp.callback_query_handler(lambda c: c.data == 'delYes')                            #   для удаления упражнения при выборе ДА
+async def process_callback_button1(callback_query: types.CallbackQuery):
+    global flag
+    await bot.answer_callback_query(callback_query.id)
+    await bot.send_message(callback_query.from_user.id, 'Упражнение удалено!')
+    bicepArray.remove(flag)
+    print("Упражнения для бицепса: ", *bicepArray)
+    await bot.send_message(callback_query.from_user.id, 'Выберите упражнение', reply_markup=muscles_keyboard())
+
+@dp.callback_query_handler(lambda c: c.data == 'delNo')                             # ничо не делает при выборе НЕТ
+async def process_callback_button1(callback_query: types.CallbackQuery):
+    await bot.send_message(callback_query.from_user.id, 'Упражнение не удалено!')
+    await bot.answer_callback_query(callback_query.id)
+    await bot.send_message(callback_query.from_user.id, 'Выберите упражнение', reply_markup=muscles_keyboard())
+
+
+
     # await callback_query.message.delete_reply_markup()
 
 
@@ -46,6 +119,17 @@ async def process_callback_button1(callback_query: types.CallbackQuery):
 async def process_callback_button2(callback_query: types.CallbackQuery):
     await bot.answer_callback_query(callback_query.id)
     await bot.send_message(callback_query.from_user.id, 'Нажата вторая кнопка!')
+
+
+
+
+
+
+
+
+
+
+
 
 
 @dp.message_handler(state=ChatGPTState.question)
--- telegram_bot/keyboards.py
+++ telegram_bot/keyboards.py
@@ -10,7 +10,7 @@ def create_reply_keyboard():
     return keyboard
 
 def muscles_keyboard():
-    inline_btn_1 = InlineKeyboardButton('Старый пидорас', callback_data='button1')
+    inline_btn_1 = InlineKeyboardButton('Бицепс', callback_data='button1')
     inline_btn_2 = InlineKeyboardButton('Трицепс', callback_data='button2')
     inline_btn_3 = InlineKeyboardButton('Плечи', callback_data='button3')
     inline_btn_4 = InlineKeyboardButton('Предпечья', callback_data='button4')
@@ -25,9 +25,19 @@ def muscles_keyboard():
     inline_kb_muscles.add(inline_btn_1, inline_btn_2, inline_btn_3, inline_btn_4, inline_btn_5, inline_btn_6, inline_btn_7, inline_btn_8, inline_btn_9, inline_btn_11, inline_btn_10)
     return inline_kb_muscles
 
+
+
 def bicep():
     inline_btn_1_bicep = InlineKeyboardButton('Упражнение 1', callback_data='button1_bicep')
     inline_btn_2_bicep = InlineKeyboardButton('Упражнение 2', callback_data='button2_bicep')
     inline_btn_3_bicep = InlineKeyboardButton('Упражнение 3', callback_data='button3_bicep')
-    inline_kb_bicep = InlineKeyboardMarkup().add(inline_btn_1_bicep, inline_btn_2_bicep, inline_btn_3_bicep)
+    inline_kb_bicep = InlineKeyboardMarkup(row_width=1)
+    inline_kb_bicep.add(inline_btn_1_bicep, inline_btn_2_bicep, inline_btn_3_bicep)
     return inline_kb_bicep
+
+def deleteExercise():
+    inline_btn_1_del = InlineKeyboardButton('Да', callback_data='delYes')
+    inline_btn_2_del = InlineKeyboardButton('Нет', callback_data='delNo')
+    inline_kb_deleteExercise = InlineKeyboardMarkup(row_width=1)
+    inline_kb_deleteExercise.add(inline_btn_1_del, inline_btn_2_del)
+    return inline_kb_deleteExercise
